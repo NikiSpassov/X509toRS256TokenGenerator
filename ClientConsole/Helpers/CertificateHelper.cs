4c468abd0fe652ae8679715067020ff2d03e542e
Nikolay Spassov <nikolay.spassov@bulpros.com>
2018-05-15 19:22:35 +0300

    First

---
+++ ClientConsole/Helpers/CertificateHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClientConsole.Helpers
+{
+    static class CertificateHelper
+    {
+        internal static X509Certificate2 GetCertificateFromThumbprint(string clientCertificateInfo)
+        {
+            if (string.IsNullOrEmpty(clientCertificateInfo))
+                return null;
+            Dictionary<string, string> par = GetCertificateThumbprintParameters(clientCertificateInfo);
+
+            return GetCertificateFromThumbprint(par);
+        }
+
+        private static X509Certificate2 GetCertificateFromThumbprint(Dictionary<string, string> clientCertificateInfo)
+        {
+            List<StoreLocation> locations = new List<StoreLocation>
+            {
+                StoreLocation.CurrentUser,
+                StoreLocation.LocalMachine
+            };
+
+            foreach (var location in locations)
+            {
+                X509Store store = new X509Store(clientCertificateInfo["store"], location);
+                store.Open(OpenFlags.ReadOnly);
+                var cert = store.Certificates.OfType<X509Certificate2>()
+                        .FirstOrDefault(x => x.Thumbprint.ToUpper() == clientCertificateInfo["thumbprint"].ToUpper());
+                store.Close();
+                if (cert != null)
+                    return cert;
+            }
+
+            return null;
+        }
+        private static Dictionary<string, string> GetCertificateThumbprintParameters(string clientCertificateInfo)
+        {
+            Dictionary<string, string> dict = clientCertificateInfo.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(part => part.Split('='))
+               .ToDictionary(split => split[0], split => split[1]);
+
+            return dict;
+        }
+    }
+}
---
+++ ClientConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using ClientConsole.Helpers;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        private const string X509ThumbPrint = "store=My;thumbprint=D0EAAFEAF821AC1C7D788D54BFAC3AD6F7DBC770";
+        private static readonly X509Certificate2 ClientCertificate = CertificateHelper.GetCertificateFromThumbprint(X509ThumbPrint);
+
+        static void Main(string[] args)
+        {
+            var token = CreateToken();
+            Console.WriteLine(token);
+
+            var securityToken = ValidateToken(token);
+            Console.WriteLine(securityToken);
+        }
+
+        public static string CreateToken() {
+            var securityKey = new X509SecurityKey(ClientCertificate);
+            var header = new JwtHeader(new SigningCredentials(securityKey, "RS256"));
+
+            var payload = new JwtPayload
+            {
+                {"iss", "issuer_id"},
+                {"scope", "scope.com"},
+                {"aud", "aud.com"},
+                { "exp", (Int32)(DateTime.UtcNow.AddHours(20).Subtract(new DateTime(1970, 1, 1))).TotalSeconds},
+                { "iat", (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds}
+            };
+
+            var jwtToken = new JwtSecurityToken(header, payload);
+            var handler = new JwtSecurityTokenHandler();
+
+            return handler.WriteToken(jwtToken);
+        }
+
+        public static SecurityToken ValidateToken(string tokenToValidate)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            SecurityToken token;
+            
+            try
+            {
+                ClaimsPrincipal principal = handler.ValidateToken(tokenToValidate, new TokenValidationParameters
+                {
+                    ValidIssuer = "issuer_id",
+                    ValidAudience = "aud.com",
+                    IssuerSigningKey = new X509SecurityKey(ClientCertificate),
+                }, out token);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+            return token;
+        }
+    }
+}
